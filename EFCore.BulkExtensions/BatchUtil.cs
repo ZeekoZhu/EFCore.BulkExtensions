f42f296f33245c277280bcf6057d8f6ead2199df
ZeekoZhu <vaezt@outlook.com>
2019-06-17 09:56:15 +0800

    fix(BatchUtils): handle different table escape symbol

--- EFCore.BulkExtensions/BatchUtil.cs
+++ EFCore.BulkExtensions/BatchUtil.cs
@@ -14,6 +14,25 @@ namespace EFCore.BulkExtensions
 {
     static class BatchUtil
     {
+        public static (string Left, string Right) GetTableEscapeSymbol(string sql)
+        {
+            var firstChar = sql[7];
+            switch (firstChar)
+            {
+                // MySQL
+                case '`':
+                // SQL Standard
+                case '"':
+                    var str = firstChar.ToString();
+                    return (str, str);
+                // MS SQL
+                case '[':
+                    return ("[", "]");
+                // https://github.com/borisdj/EFCore.BulkExtensions/issues/132#issuecomment-455810601
+                default:
+                    return (string.Empty, string.Empty);
+            }
+        }
         // In comment are Examples of how SqlQuery is changed for Sql Batch
 
         // SELECT [a].[Column1], [a].[Column2], .../r/n
@@ -25,8 +44,8 @@ namespace EFCore.BulkExtensions
         // WHERE [a].[Columns] = FilterValues
         public static string GetSqlDelete<T>(IQueryable<T> query) where T : class
         {
-            (string sql, string tableAlias) = GetBatchSql(query);
-            return $"DELETE [{tableAlias}]{sql}";
+            (string sql, string tableAlias, var (left, right)) = GetBatchSql(query);
+            return $"DELETE {left}{tableAlias}{right}{sql}";
         }
 
         // SELECT [a].[Column1], [a].[Column2], .../r/n
@@ -36,12 +55,16 @@ namespace EFCore.BulkExtensions
         // UPDATE [a] SET [UpdateColumns] = N'updateValues'
         // FROM [Table] AS [a]
         // WHERE [a].[Columns] = FilterValues
-        public static (string, List<SqlParameter>) GetSqlUpdate<T>(IQueryable<T> query, DbContext context, T updateValues, List<string> updateColumns) where T : class, new()
+        public static (string, List<SqlParameter>) GetSqlUpdate<T>(
+            IQueryable<T> query,
+            DbContext context,
+            T updateValues,
+            List<string> updateColumns) where T : class, new()
         {
-            (string sql, string tableAlias) = GetBatchSql(query);
+            (string sql, string tableAlias, var (left, right)) = GetBatchSql(query);
             var sqlParameters = new List<SqlParameter>();
             string sqlSET = GetSqlSetSegment(context, updateValues, updateColumns, sqlParameters);
-            return ($"UPDATE [{tableAlias}] {sqlSET}{sql}", sqlParameters);
+            return ($"UPDATE {left}{tableAlias}{right} {sqlSET}{sql}", sqlParameters);
         }
 
         /// <summary>
@@ -51,28 +74,41 @@ namespace EFCore.BulkExtensions
         /// <param name="query"></param>
         /// <param name="expression"></param>
         /// <returns></returns>
-        public static (string, List<SqlParameter>) GetSqlUpdate<T>(IQueryable<T> query, Expression<Func<T, T>> expression) where T : class
+        public static (string, List<SqlParameter>) GetSqlUpdate<T>(
+            IQueryable<T> query,
+            Expression<Func<T, T>> expression) where T : class
         {
-            (string sql, string tableAlias) = GetBatchSql(query);
+            (string sql, string tableAlias, var (left, right)) = GetBatchSql(query);
             var sqlColumns = new StringBuilder();
             var sqlParameters = new List<SqlParameter>();
-            var columnNameValueDict = TableInfo.CreateInstance(GetDbContext(query), new List<T>(), OperationType.Read, new BulkConfig()).PropertyColumnNamesDict;
+            var columnNameValueDict = TableInfo
+                .CreateInstance(GetDbContext(query), new List<T>(), OperationType.Read, new BulkConfig())
+                .PropertyColumnNamesDict;
             CreateUpdateBody(columnNameValueDict, tableAlias, expression.Body, ref sqlColumns, ref sqlParameters);
-            return ($"UPDATE [{tableAlias}] SET {sqlColumns.ToString()} {sql}", sqlParameters);
+            return ($"UPDATE {left}{tableAlias}{right} SET {sqlColumns.ToString()} {sql}", sqlParameters);
         }
 
-        public static (string, string) GetBatchSql<T>(IQueryable<T> query) where T : class
+        public static (string, string, (string Left, string Right) TableEscapeSymbol) GetBatchSql<T>(IQueryable<T> query) where T : class
         {
             string sqlQuery = query.ToSql();
-            string tableAlias = sqlQuery.Substring(8, sqlQuery.IndexOf("]") - 8);
+            var tableEscapeSymbol = GetTableEscapeSymbol(sqlQuery);
+            var leadingLength = 7 + tableEscapeSymbol.Left.Length;
+            var aliasEnding = tableEscapeSymbol.Right.Length > 0 ? tableEscapeSymbol.Right : ".";
+            var tableAlias = sqlQuery.Substring(leadingLength, sqlQuery.IndexOf(aliasEnding, StringComparison.Ordinal) - leadingLength);
             int indexFROM = sqlQuery.IndexOf(Environment.NewLine);
             string sql = sqlQuery.Substring(indexFROM, sqlQuery.Length - indexFROM);
             sql = sql.Contains("{") ? sql.Replace("{", "{{") : sql; // Curly brackets have to escaped:
-            sql = sql.Contains("}") ? sql.Replace("}", "}}") : sql; // https://github.com/aspnet/EntityFrameworkCore/issues/8820
-            return (sql, tableAlias);
+            sql = sql.Contains("}")
+                ? sql.Replace("}", "}}")
+                : sql; // https://github.com/aspnet/EntityFrameworkCore/issues/8820
+            return (sql, tableAlias, tableEscapeSymbol);
         }
 
-        public static string GetSqlSetSegment<T>(DbContext context, T updateValues, List<string> updateColumns, List<SqlParameter> parameters) where T : class, new()
+        public static string GetSqlSetSegment<T>(
+            DbContext context,
+            T updateValues,
+            List<string> updateColumns,
+            List<SqlParameter> parameters) where T : class, new()
         {
             var tableInfo = TableInfo.CreateInstance<T>(context, new List<T>(), OperationType.Read, new BulkConfig());
             string sql = string.Empty;
@@ -93,12 +129,15 @@ namespace EFCore.BulkExtensions
                     {
                         lastType = property.PropertyType;
                         property = lastType.GetProperty(pArray[i]);
-                        propertyUpdateValue = propertyUpdateValue != null ? property.GetValue(propertyUpdateValue) : propertyUpdateValue;
+                        propertyUpdateValue = propertyUpdateValue != null
+                            ? property.GetValue(propertyUpdateValue)
+                            : propertyUpdateValue;
                         var lastDefaultValues = lastType.Assembly.CreateInstance(lastType.FullName);
                         propertyDefaultValue = property.GetValue(lastDefaultValues);
                     }
 
-                    bool isDifferentFromDefault = propertyUpdateValue != null && propertyUpdateValue?.ToString() != propertyDefaultValue?.ToString();
+                    bool isDifferentFromDefault = propertyUpdateValue != null &&
+                                                  propertyUpdateValue?.ToString() != propertyDefaultValue?.ToString();
                     if (isDifferentFromDefault || (updateColumns != null && updateColumns.Contains(propertyName)))
                     {
                         sql += $"[{columnName}] = @{columnName}, ";
@@ -107,10 +146,13 @@ namespace EFCore.BulkExtensions
                     }
                 }
             }
+
             if (String.IsNullOrEmpty(sql))
             {
-                throw new InvalidOperationException("SET Columns not defined. If one or more columns should be updated to theirs default value use 'updateColumns' argument.");
+                throw new InvalidOperationException(
+                    "SET Columns not defined. If one or more columns should be updated to theirs default value use 'updateColumns' argument.");
             }
+
             sql = sql.Remove(sql.Length - 2, 2); // removes last excess comma and space: ", "
             return $"SET {sql}";
         }
@@ -129,7 +171,12 @@ namespace EFCore.BulkExtensions
         /// <param name="expression"></param>
         /// <param name="sqlColumns"></param>
         /// <param name="sqlParameters"></param>
-        public static void CreateUpdateBody(Dictionary<string, string> columnNameValueDict, string tableAlias, Expression expression, ref StringBuilder sqlColumns, ref List<SqlParameter> sqlParameters)
+        public static void CreateUpdateBody(
+            Dictionary<string, string> columnNameValueDict,
+            string tableAlias,
+            Expression expression,
+            ref StringBuilder sqlColumns,
+            ref List<SqlParameter> sqlParameters)
         {
             if (expression is MemberInitExpression memberInitExpression)
             {
@@ -144,14 +191,16 @@ namespace EFCore.BulkExtensions
 
                         sqlColumns.Append(" =");
 
-                        CreateUpdateBody(columnNameValueDict, tableAlias, assignment.Expression, ref sqlColumns, ref sqlParameters);
+                        CreateUpdateBody(columnNameValueDict, tableAlias, assignment.Expression, ref sqlColumns,
+                            ref sqlParameters);
 
                         if (memberInitExpression.Bindings.IndexOf(item) < (memberInitExpression.Bindings.Count - 1))
                             sqlColumns.Append(" ,");
                     }
                 }
             }
-            else if (expression is MemberExpression memberExpression && memberExpression.Expression is ParameterExpression)
+            else if (expression is MemberExpression memberExpression &&
+                     memberExpression.Expression is ParameterExpression)
             {
                 if (columnNameValueDict.TryGetValue(memberExpression.Member.Name, out string value))
                     sqlColumns.Append($" [{tableAlias}].[{value}]");
@@ -169,18 +218,21 @@ namespace EFCore.BulkExtensions
                 switch (unaryExpression.NodeType)
                 {
                     case ExpressionType.Convert:
-                        CreateUpdateBody(columnNameValueDict, tableAlias, unaryExpression.Operand, ref sqlColumns, ref sqlParameters);
+                        CreateUpdateBody(columnNameValueDict, tableAlias, unaryExpression.Operand, ref sqlColumns,
+                            ref sqlParameters);
                         break;
                     case ExpressionType.Not:
-                        sqlColumns.Append(" ~");//this way only for SQL Server 
-                        CreateUpdateBody(columnNameValueDict, tableAlias, unaryExpression.Operand, ref sqlColumns, ref sqlParameters);
+                        sqlColumns.Append(" ~"); //this way only for SQL Server 
+                        CreateUpdateBody(columnNameValueDict, tableAlias, unaryExpression.Operand, ref sqlColumns,
+                            ref sqlParameters);
                         break;
                     default: break;
                 }
             }
             else if (expression is BinaryExpression binaryExpression)
             {
-                CreateUpdateBody(columnNameValueDict, tableAlias, binaryExpression.Left, ref sqlColumns, ref sqlParameters);
+                CreateUpdateBody(columnNameValueDict, tableAlias, binaryExpression.Left, ref sqlColumns,
+                    ref sqlParameters);
 
                 switch (binaryExpression.NodeType)
                 {
@@ -208,7 +260,8 @@ namespace EFCore.BulkExtensions
                     default: break;
                 }
 
-                CreateUpdateBody(columnNameValueDict, tableAlias, binaryExpression.Right, ref sqlColumns, ref sqlParameters);
+                CreateUpdateBody(columnNameValueDict, tableAlias, binaryExpression.Right, ref sqlColumns,
+                    ref sqlParameters);
             }
             else
             {
@@ -223,13 +276,18 @@ namespace EFCore.BulkExtensions
         public static DbContext GetDbContext(IQueryable query)
         {
             var bindingFlags = BindingFlags.NonPublic | BindingFlags.Instance;
-            var queryCompiler = typeof(EntityQueryProvider).GetField("_queryCompiler", bindingFlags).GetValue(query.Provider);
-            var queryContextFactory = queryCompiler.GetType().GetField("_queryContextFactory", bindingFlags).GetValue(queryCompiler);
+            var queryCompiler = typeof(EntityQueryProvider).GetField("_queryCompiler", bindingFlags)
+                .GetValue(query.Provider);
+            var queryContextFactory = queryCompiler.GetType().GetField("_queryContextFactory", bindingFlags)
+                .GetValue(queryCompiler);
 
-            var dependencies = typeof(RelationalQueryContextFactory).GetProperty("Dependencies", bindingFlags).GetValue(queryContextFactory);
-            var queryContextDependencies = typeof(DbContext).Assembly.GetType(typeof(QueryContextDependencies).FullName);
-            var stateManagerProperty = queryContextDependencies.GetProperty("StateManager", bindingFlags | BindingFlags.Public).GetValue(dependencies);
-            var stateManager = (IStateManager)stateManagerProperty;
+            var dependencies = typeof(RelationalQueryContextFactory).GetProperty("Dependencies", bindingFlags)
+                .GetValue(queryContextFactory);
+            var queryContextDependencies =
+                typeof(DbContext).Assembly.GetType(typeof(QueryContextDependencies).FullName);
+            var stateManagerProperty = queryContextDependencies
+                .GetProperty("StateManager", bindingFlags | BindingFlags.Public).GetValue(dependencies);
+            var stateManager = (IStateManager) stateManagerProperty;
 
             return stateManager.Context;
         }
